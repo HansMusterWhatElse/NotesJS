f0c3cb286518bd1fba4cec51537ffd2f1f60beb9
HansMusterWhatElse <hansmuster456@outlook.com>
2015-05-27 00:21:16 +0200

    .

--- NotesJS/js/testing.js
+++ NotesJS/js/testing.js
@@ -1,20 +1,17 @@
 ﻿// Test retrieving serialized data from local storage and subsequently populate the note form
 var testingObj = {
     loadNoteToForm: function (uid) {
-
         // Create a new note
         app.createNote();
 
-        if (!helperFn.isEmpty(uid)) {
-            // Retrieve guid
-            var lastGuid = testingObj.retrieveGuid();
-            // Use the key passed as parameter, if its empty, use the one from the last serialisation
-            app.myCurrentNote.currentWorkingObject = app.myCurrentNote.deserialize(uid || lastGuid);
-            // Log the guid the console
-            Console.log("Deserialisation was done with the following key:" + (uid || lastGuid));
-            // Populate the form with the deserilised data
-            app.myCurrentNote.populateForm();
-        }
+        // Retrieve guid
+        var lastGuid = testingObj.retrieveGuid();
+        // Use the key passed as parameter, if its empty, use the one from the last serialisation
+        app.myCurrentNote.currentWorkingObject = app.myCurrentNote.deserialize(uid || lastGuid);
+        // Log the guid the console
+        Console.log("Deserialisation was done with the following key:" + (uid || lastGuid));
+        // Populate the form with the deserilised data
+        app.myCurrentNote.populateForm();
 
         $('#createNoteView').show('slow');
     },
