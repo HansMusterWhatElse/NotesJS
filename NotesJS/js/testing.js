580fdb8ffaa78416aded086751937da9c71a172a
HansMusterWhatElse <hansmuster456@outlook.com>
2015-05-27 20:02:38 +0200

    .

--- NotesJS/js/testing.js
+++ NotesJS/js/testing.js
@@ -5,14 +5,18 @@ var testingObj = {
         app.createNote();
         // Retrieve guid
         var lastGuid = testingObj.retrieveGuid();
-        // Use the key passed as parameter, if its empty, use the one from the last serialisation
-        app.myCurrentNote.currentWorkingObject = app.myCurrentNote.deserialize(uid || lastGuid);
-        // Log the guid the console
-        console.log('Deserialisation was done with the following key:' + (uid || lastGuid));
-        // Populate the form with the deserilised data
-        app.myCurrentNote.populateForm();
 
-        $('#createNoteView').show('slow');
+        if (helperFn.isEmpty(uid) && helperFn.isEmpty(lastGuid)) {
+            // Use the key passed as parameter, if its empty, use the one from the last serialisation
+            app.myCurrentNote.currentWorkingObject = app.myCurrentNote.deserialize(uid || lastGuid);
+            // Log the guid the console
+            console.log('Deserialisation was done with the following key:' + (uid || lastGuid));
+            // Populate the form with the deserilised data
+            app.myCurrentNote.populateForm();
+
+            $('#createNoteView').show('slow');
+        }
+
     },
 
     // Stores the last serialised guid from the local storage
